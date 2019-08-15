3b0b6eb08b3d0506913fbbc181afe9af0508b88d
Vibhuti Singh <44270768+vibhuti019@users.noreply.github.com>
2019-08-14 21:24:28 -0700

    Update sprint.bat

--- sprint.bat
+++ sprint.bat
@@ -1,6 +1,13 @@
 @echo off
+REM This Is A Auxillary File For view.exe
+
+REM Clear THe Screen
 cls
+
+REM Change The Color
 color E4
+
+REM Creating Temp File And Viewing It
 cd %temp%
 type tStu.bin
 echo **************************************************************************** >> tStu.bin
@@ -10,9 +17,19 @@ echo *                   Save The File And Close Notepad                        
 echo *                                                                          *
 echo *                                                                          *
 echo ****************************************************************************
+
+REM Create Temp File
 ren tStu.bin tStu.txt
+
+REM Start Notepad And Halt The Executer
 start /b /wait notepad tStu.txt
+
+REM Create Temp File
 del tStu.txt
+
+REM Exit
 pause
 cd %appdata%/StuDBMS/SoftFiles
+
+REM Open Print Again
 print
